df04db8775d6b9c7242ab088a6461e04beb119d1
Brian Lo <darkharry25@gmail.com>
2021-02-04 23:57:42 +0800

    新增範例程式碼
    新增chapter03 part 1

---
+++ src/main/java/concrurrency/chapter03/exit/InterruptThreadExit.java
@@ -0,0 +1,23 @@
+package concrurrency.chapter03.exit;
+
+import java.util.concurrent.TimeUnit;
+
+public class InterruptThreadExit {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+               System.out.println("I will start work");
+               while (!isInterrupted()){
+
+               }
+                System.out.println("I will be existing");
+            }
+        };
+
+        t.start();
+        TimeUnit.MINUTES.sleep(1);
+        System.out.println("System will be shutdown");
+        t.interrupt();
+    }
+}

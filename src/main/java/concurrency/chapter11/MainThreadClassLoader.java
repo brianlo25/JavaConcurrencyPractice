2d05bc3cf4d28812c5760fdd112cfcc26795ea6c
Brian Lo <darkharry25@gmail.com>
2021-04-01 01:17:12 +0800

    新增範例程式碼
    新增chapter11

---
+++ src/main/java/concurrency/chapter11/MainThreadClassLoader.java
@@ -0,0 +1,7 @@
+package concurrency.chapter11;
+
+public class MainThreadClassLoader {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getContextClassLoader());
+    }
+}

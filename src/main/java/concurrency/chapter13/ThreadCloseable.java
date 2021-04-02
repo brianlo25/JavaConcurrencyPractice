344b71557061c2c248482acd9a76bd866a9b9032
Brian Lo <darkharry25@gmail.com>
2021-04-02 23:43:28 +0800

    新增範例程式碼
    新增chapter12、13

---
+++ src/main/java/concurrency/chapter13/ThreadCloseable.java
@@ -0,0 +1,16 @@
+package concurrency.chapter13;
+
+public class ThreadCloseable extends Thread{
+    private volatile boolean started = true;
+
+    @Override
+    public void run() {
+        while (started){
+            
+        }
+    }
+
+    public void shutdown(){
+        this.started = false;
+    }
+}

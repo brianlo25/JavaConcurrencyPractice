e36ea6c4dfba8de85ed7c83a6702ced2c6261641
Brian Lo <darkharry25@gmail.com>
2021-07-09 16:43:08 +0800

    新增範例程式碼
    新增AtomicReference範例

---
+++ src/main/java/concurrentcore/atomicreference/AtomicReferenceExample1.java
@@ -0,0 +1,30 @@
+package concurrentcore.atomicreference;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class AtomicReferenceExample1 {
+    static volatile DebitCard debitCard = new DebitCard("Alex", 0);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            new Thread("T-" + i){
+                @Override
+                public void run() {
+                   while (true){
+                       final DebitCard dc = debitCard;
+                       DebitCard newDC = new DebitCard(dc.getAccount(), dc.getAmount() + 10);
+                       System.out.println(newDC);
+                       debitCard = newDC;
+
+                       try {
+                           TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(10));
+                       } catch (InterruptedException e) {
+                           e.printStackTrace();
+                       }
+                   }
+                }
+            }.start();
+        }
+    }
+}

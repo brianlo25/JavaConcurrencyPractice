f46e918dc6e4715a55046f84aee82e2592647114
Brian Lo <darkharry25@gmail.com>
2022-04-17 23:27:42 +0800

    新增範例程式碼
    新增semaphore範例

---
+++ src/main/java/javaconcurrency/chapter06/semaphore/SemaphoreExample.java
@@ -0,0 +1,35 @@
+package javaconcurrency.chapter06.semaphore;
+
+import java.util.concurrent.*;
+
+public class SemaphoreExample {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        ExecutorService service = Executors.newCachedThreadPool();
+        for (int i = 0; i < 1000; i++) {
+            service.execute(new SomeTask(semaphore));
+        }
+    }
+
+    static class SomeTask implements Runnable {
+        private Semaphore semaphore;
+
+        public SomeTask(Semaphore semaphore) {
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + "獲得一個令牌");
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                System.out.println(Thread.currentThread().getName() + "釋放一個令牌");
+                semaphore.release();
+            }
+        }
+    }
+}

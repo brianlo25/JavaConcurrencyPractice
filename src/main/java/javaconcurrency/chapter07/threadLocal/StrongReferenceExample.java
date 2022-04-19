d5aad676984330be10bb8abaef109daf4fca71a1
Brian Lo <darkharry25@gmail.com>
2022-04-19 23:43:31 +0800

    新增範例程式碼
    新增ThreadLocal範例

---
+++ src/main/java/javaconcurrency/chapter07/threadLocal/StrongReferenceExample.java
@@ -0,0 +1,12 @@
+package javaconcurrency.chapter07.threadLocal;
+
+public class StrongReferenceExample {
+    static Object object = new Object();
+
+    public static void main(String[] args) {
+        Object strongRef = object;
+        object = null;
+        System.gc();
+        System.out.println("gc之後:" + strongRef);
+    }
+}

8d3930af337d0c5409ea35f4c2cc213b6a114b0b
brianlo25 <30383913+brianlo25@users.noreply.github.com>
2022-04-19 17:37:20 +0800

    新增範例程式碼
    新增ThreadLocal範例

---
+++ src/main/java/javaconcurrency/chapter07/threadLocal/SimpleDateFormatExample.java
@@ -0,0 +1,28 @@
+package javaconcurrency.chapter07.threadLocal;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleDateFormatExample {
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static Date parse(String strDate) throws ParseException {
+        return sdf.parse(strDate);
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 9; i++) {
+            executorService.execute(() -> {
+                try {
+                    System.out.println(parse("2022-04-19 17:12:30"));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}

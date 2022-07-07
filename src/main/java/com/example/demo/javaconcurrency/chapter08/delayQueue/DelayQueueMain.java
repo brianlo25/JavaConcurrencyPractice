26918a8d253700dc5eb9f81f28cb5aed958c92fd
brianlo25 <30383913+brianlo25@users.noreply.github.com>
2022-07-07 16:56:25 +0800

    新增範例程式碼
    新增DelayQueue範例

---
+++ src/main/java/com/example/demo/javaconcurrency/chapter08/delayQueue/DelayQueueMain.java
@@ -0,0 +1,29 @@
+package com.example.demo.javaconcurrency.chapter08.delayQueue;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+
+@Slf4j
+public class DelayQueueMain {
+    private static DelayQueue delayQueue = new DelayQueue();
+
+    public static void main(String[] args) {
+        delayQueue.offer(new OrderDelayedTask("1001", 1000));
+        delayQueue.offer(new OrderDelayedTask("1002", 3000));
+        delayQueue.offer(new OrderDelayedTask("1003", 2000));
+        delayQueue.offer(new OrderDelayedTask("1004", 4000));
+        delayQueue.offer(new OrderDelayedTask("1005", 6000));
+        delayQueue.offer(new OrderDelayedTask("1006", 5000));
+
+        while (true) {
+            try {
+                Delayed task = delayQueue.take();
+                log.info("{}", task);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/javaconcurrency/chapter08/delayQueue/OrderDelayedTask.java
@@ -0,0 +1,35 @@
+package com.example.demo.javaconcurrency.chapter08.delayQueue;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class OrderDelayedTask implements Delayed {
+
+    private String orderId;
+    private long start = System.currentTimeMillis();
+    private long time;
+
+    public OrderDelayedTask(String orderId, long time) {
+        this.orderId = orderId;
+        this.time = time;
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return unit.convert((start + time) - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        return (int) (this.getDelay(TimeUnit.MILLISECONDS) - o.getDelay(TimeUnit.MILLISECONDS));
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDelayedTask{" +
+                "orderId='" + orderId + '\'' +
+                ", start=" + start +
+                ", time=" + time +
+                '}';
+    }
+}
